d025a7a770b7b79e9e66734047f548004e35d32a
kiran Bhujbal <kiran.bhujbal@bitstreet.co.in>
2019-06-20 19:31:07 +0530

    Project setup

---
+++ imports/ui/body.js
@@ -0,0 +1,25 @@
+
+
+import { Template } from 'meteor/templating';
+
+ 
+
+import './body.html';
+
+ 
+
+Template.body.helpers({
+
+  tasks: [
+
+    { text: 'This is task 1' },
+
+    { text: 'This is task 2' },
+
+    { text: 'This is task 3' },
+    { text: 'This is task 4' },
+    { text: 'This is task 5' },
+  ],
+
+});
+
